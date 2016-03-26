872d8fea620a8b8d25511d58efea824ef4a816ee
ganapathy93 <ganapathyram93@gmail.com>
2016-03-26 01:31:25 -0500

    image conversion and responsive

--- dist/js/wow.js
+++ dist/js/wow.js
@@ -3,6 +3,8 @@
     bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
+	
+	
   Util = (function() {
     function Util() {}
 
